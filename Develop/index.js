4913eb06220804971648b9c46c52f0c1097dbc35
nosremetnarg <grant.alex.emerson@gmail.com>
2020-07-31 21:46:54 -0500

    another minor adjustment

--- Develop/index.js
+++ Develop/index.js
@@ -47,7 +47,7 @@ const questions = [
         type: 'checkbox',
         name: 'languages',
         message: 'What did you this project with? (Check all that apply)',
-        choices: ['JavaScript ', 'HTML ', 'CSS ', 'ES6 ', 'jQuery ', 'Bootstrap ', 'Node ']
+        choices: ['JavaScript ' , 'HTML ' , 'CSS ' , 'ES6 ' , 'jQuery ' , 'Bootstrap ' , 'Node ']
     },
     {
         type: 'input',
